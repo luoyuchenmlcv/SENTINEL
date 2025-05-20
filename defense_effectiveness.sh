2b03a48b6d8f58db48cd57fb8226cf951d8eca43
luoyuchenmlcv <102636806+luoyuchenmlcv@users.noreply.github.com>
2025-05-21 00:19:31 +1000

    Update defense_effectiveness.sh

--- defense_effectiveness.sh
+++ defense_effectiveness.sh
@@ -1,10 +1,10 @@
 #llama2_7b
-python pipeline_sentinel_gen.py --victim_model llama2_7b --harm_thresh 3.3
+python pipeline_sentinel_gen.py --victim_model llama2_7b --harm_thresh 3.25
 python attack_success_rate.py --victim_model llama2_7b
-python orbench-sentinel.py --victim_model llama2_7b --harm_thresh 3.3
+python orbench-sentinel.py --victim_model llama2_7b --harm_thresh 3.25
 
 
-# #mistral_7b_v2 #0.4
+# #mistral_7b_v2 
 python pipeline_sentinel_gen.py --victim_model mistral_7b_v2 --harm_thresh 1.1
 python attack_success_rate.py --victim_model mistral_7b_v2
 python orbench-sentinel.py --victim_model mistral_7b_v2 --harm_thresh 1.1
